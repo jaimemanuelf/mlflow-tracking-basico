fe608b23acc7dcc583af80ea29d69d9892a815eb
Jaime Manuel Figueroa <99105750+jaimemanuelf@users.noreply.github.com>
2026-04-10 22:51:22 -0500

    Initial commit

---
+++ make_predictions.py
@@ -0,0 +1,58 @@
+#
+#
+#
+#### MAKE_PREDICTIONS ####
+
+"""Prediccion script for the MLflow model.
+
+This script loads a model from MLflow and makes predictions on a dataset.
+
+$ python3 make_predictions.py
+
+
+"""
+
+import mlflow
+import pandas as pd
+
+FILE_PATH = "data/winequality-red.csv"
+
+
+df = pd.read_csv(FILE_PATH)
+y = df["quality"]
+x = df.drop(columns=["quality"])
+
+## Debe verificarse el run_id del modelo que se quiere cargar
+## Se puede obtener el run_id desde la interfaz de MLflow
+
+logged_model = "runs:/569cd274ba0d46728a0bd53719c85915/model"
+loaded_model = mlflow.pyfunc.load_model(logged_model)
+y = loaded_model.predict(x)
+
+print(y)
+
+
+# #prediction script for de mlflow model
+
+# #this script loads a trained model from MLflow and makes predictions on new data.
+
+# #$ python3 make_predictions.py
+
+# import mlflow
+# import pandas as pd
+
+# FILE_PATH = "data/winequality-red.csv"
+
+# df = pd.read_csv(FILE_PATH)
+
+# y = df["quality"]
+# X = df.drop(columns=["quality"])
+
+
+# ## debe verificar el nombre del experimento y el run_id del modelo que desea cargar
+
+# logged_model = 'runs:/<run_id>/model'  ## replace <run_id> with the actual run id
+
+# logged_model = mlflow.pyfunc.load_model(logged_model)
+
+# y =
